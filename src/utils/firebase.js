c241e5b46236a1d774187aedc46bd90c4ab5496a
Pineapple-Soup <91655764+Pineapple-Soup@users.noreply.github.com>
2025-01-26 02:37:02 -0800

    build: initialize firebase authentication

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -12,7 +13,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const firebaseApp =
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
-export { db };
+export const db = getFirestore(firebaseApp);
+export const auth = getAuth(firebaseApp);
